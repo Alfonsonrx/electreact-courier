deb86e7af90078c841cd5abd503b2e6f16c2389d
BuildTools <damian.contreras036@gmail.com>
2023-07-17 03:23:40 -0400

    Start of courier system with electron-react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+
+const App = () => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    return (
+      <BrowserRouter basename={'/'}>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/settings" element={<Settings/>} />
+        </Routes>
+      </BrowserRouter>)
+  } else {
+    return (
+      <HashRouter basename={'/'}>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/settings" element={<Settings/>} />
+        </Routes>
+      </HashRouter>)
+  }
+}
+
+export default App;
